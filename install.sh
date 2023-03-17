4f2436d65af767ed69a560fbec2135da412f19a6
Jonathan Platzer <platzerjonathan@gmail.com>
2023-03-17 10:48:42 +0200

    Add output to script

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Installing needed packages"
 apt install figlet lsb-release python3 python3-utmp bc needrestart
+echo ""
 
+echo "Copying files and creating symlinks"
 cp -r update-motd.d/ /etc
 rm /etc/motd
 ln -s /var/run/motd /etc/motd
+echo "DONE"
